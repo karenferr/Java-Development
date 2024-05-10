6c24b948ed8d14d94d7310efadd4e137c54d13e4
Karen Ferreira <karenvsferreira@outlook.com>
2024-05-09 21:50:54 -0300

    b1177

---
+++ src/A_algorithms/beeCrowd/b1177.java
@@ -0,0 +1,25 @@
+package A_algorithms.beeCrowd;
+import java.util.Scanner;
+
+public class b1177 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        int[] N = new int[1000];
+
+
+        for (int i = 0; i < N.length; i++) {
+            N[i] = i % T;
+        }
+
+        for (int i = 0; i < N.length; i++) {
+            System.out.println("N[" + i + "] = " + N[i]);
+
+        }
+        sc.close();
+    }
+}
+
+
