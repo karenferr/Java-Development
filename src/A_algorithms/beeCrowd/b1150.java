13611d656803eb38846676e6275ec11f5cba544f
Karen Ferreira <karenvsferreira@outlook.com>
2024-05-15 19:50:30 -0300

    b1150

---
+++ src/A_algorithms/beeCrowd/b1150.java
@@ -0,0 +1,27 @@
+package A_algorithms.beeCrowd;
+import java.util.Scanner;
+
+public class b1150 {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt();
+        int Z = sc.nextInt();
+
+        while (Z<=X) {
+            Z = sc.nextInt();
+        }
+
+        int soma = 0;
+        int contador = 0;
+
+        for (int i=X; soma<=Z; i++) {
+            soma += i;
+            contador++;
+        }
+
+        System.out.println(contador);
+
+        sc.close();
+    }
+}
