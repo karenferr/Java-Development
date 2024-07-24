c88ea0128199df8e31f07189c823100302cd5adb
Karen Ferreira <karenvsferreira@outlook.com>
2024-07-23 21:10:49 -0300

    b1075 b1098

---
+++ src/beeCrowd/b1075.java
@@ -0,0 +1,21 @@
+package beeCrowd;
+
+import java.util.Scanner;
+
+public class b1075 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        if (N<10000) {
+            for (int i = 1; i < 10000; i++) {
+                if (i%N==2) {
+                    System.out.println(i);
+                }
+            }
+        }
+        sc.close();
+    }
+}
---
+++ src/beeCrowd/b1098.java
@@ -0,0 +1,39 @@
+package beeCrowd;
+
+public class b1098 {
+    public static void main(String[] args) {
+        for (int j = 0; j < 3; j++) {
+            System.out.println("I=0 " + "J=" + (j+1));
+        }
+        for (double j = 0.2; j < 3; j++) {
+            System.out.println("I=0.2 " + "J=" + (j+1));
+        }
+        for (double j = 0.4; j < 3; j++) {
+            System.out.println("I=0.4 " + "J=" + (j+1));
+        }
+        for (double j = 0.6; j < 3; j++) {
+            System.out.println("I=0.6 " + "J=" + (j+1));
+        }
+        for (double j = 0.8; j < 3; j++) {
+            System.out.println("I=0.8 " + "J=" + (j+1));
+        }
+        for (int j = 1; j < 4; j++) {
+            System.out.println("I=1 " + "J=" + (j+1));
+        }
+        for (double j = 1.2; j < 4; j++) {
+            System.out.println("I=1.2 " + "J=" + (j+1));
+        }
+        for (double j = 1.4; j < 4; j++) {
+            System.out.println("I=1.4 " + "J=" + (j+1));
+        }
+        for (double j = 1.6; j < 4; j++) {
+            System.out.println("I=1.6 " + "J=" + (j+1));
+        }
+        for (double j = 1.8; j < 4; j++) {
+            System.out.println("I=1.8 " + "J=" + (j+1));
+        }
+        for (int j = 2; j < 5; j++) {
+            System.out.println("I=2 " + "J=" + (j+1));
+        }
+    }
+}
