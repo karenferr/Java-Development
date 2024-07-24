e9aa17c478503a621ed9261ba9831707630f70d7
Karen Ferreira <karenvsferreira@outlook.com>
2024-07-24 18:45:05 -0300

    b2581 b2747

---
+++ src/beeCrowd/b2581.java
@@ -0,0 +1,18 @@
+package beeCrowd;
+
+import java.util.Scanner;
+
+public class b2581 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+           String frase = sc.next();
+            System.out.println("I am Toorg!");
+        }
+
+        sc.close();
+    }
+}
---
+++ src/beeCrowd/b2747.java
@@ -0,0 +1,13 @@
+package beeCrowd;
+
+public class b2747 {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("---------------------------------------");
+    }
+}
